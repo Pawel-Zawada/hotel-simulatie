6cfcc4f6921380f1879901e45191b89a60d43f52
Johan Geluk <johan@geluk.io>
2019-09-15 20:34:40 +0200

    Add libraries and sample test (#4)

---
+++ src/test/java/EntryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class EntryTest {
+
+    @Test
+    public void main() {
+        assertThat("test").isEqualTo("test");
+    }
+}
