795c7b50ea8bbef0583933257d9564e4a21f5f7b
Umit Tokmak <44272098+umittokmak42@users.noreply.github.com>
2019-10-01 23:19:34 +0200

    modified TaskRepository

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,12 @@
 public class Task {
+    private int taskNumber;
 
+    public Task(int taskNumber){
+        this.taskNumber = taskNumber;
+    }
+
+    public int getTaskNumber() {
+        return taskNumber;
+    }
 }
 
--- src/main/java/TaskRepository.java
+++ src/main/java/TaskRepository.java
@@ -1,57 +1,44 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class TaskRepository {
-    private Task[] taskQueue;
-    private int queueSize;
-    private int head = 0;
-    private int rear = 0;
-    private int numberOfTasks = 0;
+    private ArrayList<Task> taskQueue;
 
-    TaskRepository(int size) {
-        this.queueSize = size;
-        taskQueue = new Task[size];
-        Arrays.fill(taskQueue, "-1");
+    public TaskRepository() {
+        this.taskQueue = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return taskQueue.size();
     }
 
     public void enQueue(Task newTask) {
-        if (numberOfTasks + 1 <= queueSize) {
-            taskQueue[rear] = newTask;
-            rear++;
-            numberOfTasks++;
-        }
+        this.taskQueue.add(newTask);
     }
 
     public void deQueue() {
-        if (numberOfTasks > 0) {
-            taskQueue[head] = null;
-            head++;
-            numberOfTasks--;
-        }
-    }
-
-    public void peek() {
-        System.out.println(taskQueue[head]);
-    }
-
-    public void emergencyTask(Task newTask) {
-        int i = 0;
-
-        if (numberOfTasks == 0) {
-            enQueue(newTask);
+        if (taskQueue.size() > 0) {
+            this.taskQueue.remove(0);
         } else {
-            for (i = numberOfTasks - 1; i >= 0; i--) {
-//                Task temp1 = new Task();
-//                Task temp2 = new Task();
-//                temp1 = taskQueue[head];
-//                taskQueue[head] = newTask;
-//                numberOfTasks++;
-//                temp2 = taskQueue[i + 1];
-//                taskQueue[i + 1] = temp1;
-                taskQueue[i + 1] = taskQueue[i];
-            }
-            taskQueue[i + 1] = newTask;
-            rear++;
-            numberOfTasks++;
+            System.out.println("No Tasks to dequeue");
         }
     }
+
+    public Task peek(){
+        return taskQueue.get(0);
+    }
+
+    public void addEmergencyTask(Task emergencyTask){
+        if(taskQueue.size() == 0){
+            enQueue(emergencyTask);
+        } else {
+            taskQueue.add(0,emergencyTask);
+        }
+    }
+
+    public void element(){
+        taskQueue.forEach(task -> System.out.println(task.getTaskNumber()));
+    }
+
+
 }
+
