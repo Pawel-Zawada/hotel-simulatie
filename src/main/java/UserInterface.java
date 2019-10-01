025c78b3bf6789b4f1bb5be21ff129cf651b7e20
Pawel Zawada <zawadapawelmail@gmail.com>
2019-10-01 13:46:17 +0200

    Split container & component declarations into nested inner classes
    * Declare `mainPanel` into separate custom class
    * Declare `navigationPanel` into separate custom class
      * Move exit button declaration to inner class of parent panel
      * Display exit button on the right side
    * Declare `contentPanel` into separate custom class
      * Move label declaration to inner class of parent panel

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -8,20 +8,24 @@ import java.awt.*;
  * For example: Changing HTE's, switching floors, etc.
  */
 class UserInterface extends JFrame {
-    private final JFrame frame = new JFrame("Hotel Simulator");
+    private final JFrame frame = new JFrame("Hotel Simulator"); // Main frame to contain the menu panels.
 
-    private final JPanel mainPanel = new JPanel(new BorderLayout()); // Container panel to display subcomponents like navigation and content.
-    private final JPanel navigationPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 16, 8)); // Will act as the container for navigational buttons.
-    private final JPanel contentPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 16, 8)); // Contextual content of the currently selected navigation. (For example `settings`)
-
-    private final JLabel label = new JLabel("<html><font color='white'>Testing label text</font></html>"); // HTML FTW
-    private final JButton exitButton = new ExitButton("Exit");
+    /**************
+     * Containers *
+     **************/
+    private final MainPanel mainPanel = new MainPanel(); // Container panel to display subcomponents like navigation and content.
+    private final NavigationPanel navigationPanel = new NavigationPanel(); // Will act as the container for navigational buttons.
+    private final ContentPanel contentPanel = new ContentPanel(); // Contextual content of the currently selected navigation. (For example `settings`)
 
     /**
      * Build frame & containing panels with respective components.
      */
     private void setup() {
-        configurePanels();
+        // Set `mainPanel` as the container to all components of the interface.
+        mainPanel.add(navigationPanel, BorderLayout.PAGE_START);
+        mainPanel.add(contentPanel, BorderLayout.CENTER);
+
+        frame.setContentPane(mainPanel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -29,35 +33,76 @@ class UserInterface extends JFrame {
         frame.setUndecorated(true); // ...*without* window bar.
     }
 
-    /**
-     * Configure panel setup within the frame, including the panel's components like buttons.
-     */
-    private void configurePanels() {
-        navigationPanel.setPreferredSize(new Dimension(0, 45)); // Low height so it acts as a sort of `toolbar` at the top.
-        navigationPanel.add(exitButton); // TODO: Move this to the right side of the panel.
-        navigationPanel.setBackground(Color.red);
-
-        contentPanel.add(label);
-        contentPanel.setBackground(Color.blue);
-
-        // Set `mainPanel` as the container to all components of the interface.
-        mainPanel.add(navigationPanel, BorderLayout.NORTH);
-        mainPanel.add(contentPanel, BorderLayout.CENTER);
-
-        frame.setContentPane(mainPanel);
-    }
-
     UserInterface() {
         setup();
         frame.setVisible(true);
     }
 
-    static class ExitButton extends JButton {
-        ExitButton(String text) {
-            super(text); // Set title using JButton's constructor.
+    /**********************************************
+     * Custom containers w/ respective components *
+     **********************************************/
 
-            setBackground(Color.white); // Make it white to remove the ugly metallic gradient.
-            addActionListener(e -> System.exit(0)); // Kill the program on button click.
+    private static class MainPanel extends JPanel {
+        MainPanel() {
+            super(new BorderLayout());
+        }
+    }
+
+    private static class NavigationPanel extends JPanel {
+        Dimension dimension = new Dimension(80, 40);
+        ExitPanel exitPanel = new ExitPanel(dimension);
+
+        NavigationPanel() {
+            super(new BorderLayout());
+
+            setPreferredSize(dimension);
+            setBackground(Color.red); // TODO: Remove this panel marker.
+
+            add(exitPanel, BorderLayout.LINE_END);
+        }
+
+        private static class ExitButton extends JButton {
+
+            /**
+             * @param text      Text to be displayed in the button
+             * @param dimension Button's width & height size
+             * @see JButton
+             */
+            ExitButton(String text, Dimension dimension) {
+                super(text); // Set button's text with JButton's constructor.
+
+                setPreferredSize(new Dimension(
+                        dimension.width,
+                        dimension.height - 10
+                )); // Same size as containing panel, minus 10 pixels in height margin.
+
+                setBackground(Color.white); // Make it white to remove the ugly metallic gradient.
+                addActionListener(e -> System.exit(0)); // Kill the program on button click.
+            }
+        }
+
+        private class ExitPanel extends JPanel {
+            ExitButton exitButton = new ExitButton("Exit", dimension);
+
+            /**
+             * @param dimension Panel's width & height size
+             */
+            ExitPanel(Dimension dimension) {
+                super(new FlowLayout(FlowLayout.RIGHT));
+
+                setBackground(Color.orange); // TODO: Remove this panel marker.
+                add(exitButton);
+            }
+        }
+    }
+
+    private static class ContentPanel extends JPanel {
+        ContentPanel() {
+            super(new FlowLayout(FlowLayout.CENTER, 16, 8));
+
+            JLabel label = new JLabel("<html><font color='white'>Testing label text</font></html>");
+            add(label);
+            setBackground(Color.blue); // TODO: Remove this panel marker.
         }
     }
 }
