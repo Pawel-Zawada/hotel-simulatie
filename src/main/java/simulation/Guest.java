3a3f4f0a21a32da8c2d462027fd9ab52b36d4df3
Umit Tokmak <44272098+umittokmak42@users.noreply.github.com>
2019-10-08 19:53:09 +0200

    Add guest list to `Hotel` class & add get tasks method

--- src/main/java/simulation/Guest.java
+++ src/main/java/simulation/Guest.java
@@ -1,6 +1,21 @@
 package simulation;
 
 public class Guest implements IObserver {
+    private String name;
+    private static int guestID = 00;
+
+    public Guest(){
+        guestID++;
+        this.name = "Guest: " + guestID;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getGuestID() {
+        return guestID;
+    }
+
     public void observe() {
         System.out.println("I have been notified");
     }
