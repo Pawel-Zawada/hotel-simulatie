ae3af3dbf354c569aff2cc28eb975887090332aa
Umit Tokmak <44272098+umittokmak42@users.noreply.github.com>
2019-10-09 01:28:26 +0200

    Modified DataCollector

--- src/main/java/DataCollector.java
+++ src/main/java/DataCollector.java
@@ -9,4 +9,35 @@ public class DataCollector {
     public static int getNumberOfTasks() {
         return TaskRepository.getTaskQueue().size();
     }
+
+    public static int getNumberOfOneStarRooms(){
+        return -1;
+    }
+
+    public static int getNumberOfTwoStarRooms(){
+        return -1;
+    }
+
+    public static int getNumberOfThreeStarRooms(){
+        return -1;
+    }
+
+    public static int getNumberOfFourStarRooms(){
+        return -1;
+    }
+
+    public static int getNumberOfFiveStarRooms(){
+        return -1;
+    }
+
+    public static int guestsInGym(){
+        return -1;
+    }
+
+    public static int guestsInElevator(){
+        return -1;
+    }
+
+
+
 }
