bb0e9c5afdf7c81785f9b0366e2d8ef8232f9307
prepless <42713054+prepless@users.noreply.github.com>
2019-10-09 19:56:19 +0200

    implement that A* and Dykstra are supported in graph

--- src/main/java/pathfinding/Graph.java
+++ src/main/java/pathfinding/Graph.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 public class Graph {
     private ArrayList<HotelElement> hotelElements;
     private int width;
-    private int height;
     private Hotel hotel;
     HashMap<HotelElement, Node> hotelElementNodeHashMap = new HashMap<>();
 
@@ -15,14 +14,14 @@ public class Graph {
         this.hotel = hotel;
         this.hotelElements = hotel.getHotelElements();
         this.width = hotel.getWidth();
-        this.height = hotel.getHeight();
         fillHotelElementNodeHashMap();
         setHotelElementNeighbours();
     }
 
     private void fillHotelElementNodeHashMap(){
         for(HotelElement hotelElement:hotelElements){
-           hotelElementNodeHashMap.put( hotelElement,new Node(hotelElement.getX(),hotelElement.getY()));
+            /** Nodes get a X and a Y just in case we want to use A* **/
+            hotelElementNodeHashMap.put( hotelElement,new Node(hotelElement.getX(),hotelElement.getY()));
         }
     }
 
@@ -30,7 +29,7 @@ public class Graph {
         for(HotelElement hotelElement:hotelElementNodeHashMap.keySet()){
             if (hotelElement.getClass() == Elevator.class){
                 for(Node node:hotelElementNodeHashMap.values()){
-                    if (hotelElement.getX()+hotelElement.getWidth() == node.getX()) {
+                    if (hotelElement.getX()+hotelElement.getWidth() == getElementOfNode(node).getX()) {
                         hotelElementNodeHashMap.get(hotelElement).connect(node, hotel.getElevatorWeight());
                     }
                 }
@@ -39,9 +38,9 @@ public class Graph {
                 int xOfLeftNode = 0;
                 Node leftNode = null;
                 for(Node node:hotelElementNodeHashMap.values()){
-                    if (hotelElement.getY()==node.getY()) { //same floor
-                        if (xOfLeftNode<node.getX() && node.getX() != hotelElement.getX()){
-                            xOfLeftNode = node.getX();
+                    if (hotelElement.getY()==getElementOfNode(node).getY()) { //same floor
+                        if (xOfLeftNode<getElementOfNode(node).getX() && getElementOfNode(node).getX() != hotelElement.getX()){
+                            xOfLeftNode = getElementOfNode(node).getX();
                             leftNode = node;
                         }
                     }
@@ -52,23 +51,23 @@ public class Graph {
                 }
                 //connect top and bottom nodes
                 for(Node node:hotelElementNodeHashMap.values()){
-                    if (hotelElement.getX()==node.getX()) { //node is a stairs
-                        if (node.getY() == hotelElement.getY()+1 || node.getY() == hotelElement.getY()-1){
+                    if (hotelElement.getX()==getElementOfNode(node).getX()) { //node is a stairs
+                        if (getElementOfNode(node).getY() == hotelElement.getY()+1 || getElementOfNode(node).getY() == hotelElement.getY()-1){
                             hotelElementNodeHashMap.get(hotelElement).connect(node, hotel.getStairsWeight());
                         }
                     }
                 }
             } else{
                 for (Node node:hotelElementNodeHashMap.values()){
-                    if(node.getX() != 0 && node.getX() != width-1) {
-                        if (node.getY() == hotelElement.getY()) { //same floor
-                            if (node.getX() == hotelElement.getX() + hotelElement.getWidth()) { //connect right node
+                    if(getElementOfNode(node).getX() != 0 && getElementOfNode(node).getX() != width-1) {
+                        if (getElementOfNode(node).getY() == hotelElement.getY()) { //same floor
+                            if (getElementOfNode(node).getX() == hotelElement.getX() + hotelElement.getWidth()) { //connect right node
                                 int weight = hotelElement.getX() + hotelElement.getWidth();
                                 hotelElementNodeHashMap.get(hotelElement).connect(node, weight);
                             }
 
-                            if ((hotelElement.getX()- getElementOfNode(node).getWidth()) == node.getX()){ //connect left node
-                                int weight = hotelElement.getX() - node.getX();
+                            if ((hotelElement.getX()- getElementOfNode(node).getWidth()) == getElementOfNode(node).getX()){ //connect left node
+                                int weight = hotelElement.getX() - getElementOfNode(node).getX();
                                 hotelElementNodeHashMap.get(hotelElement).connect(node, weight);
                             }
                         }
@@ -81,7 +80,7 @@ public class Graph {
     private HotelElement getElementOfNode(Node node) {
         HotelElement hotelElement = null;
         for (HotelElement tmpElement: hotelElementNodeHashMap.keySet()){
-            if ((tmpElement.getY() == node.getY()) && (tmpElement.getX()==node.getX())){
+            if (hotelElementNodeHashMap.get(tmpElement) == node){
                 hotelElement = tmpElement;
             }
         }
